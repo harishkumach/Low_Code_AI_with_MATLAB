2f4694cf6639b2923048e205cb961b2c45104b76
harishkumach <134656610+harishkumach@users.noreply.github.com>
2024-07-30 16:44:53 -0700

    Add files via upload

---
+++ scatterUsingValues.m
@@ -0,0 +1,10 @@
+function scatterUsingValues(X,Y,V) 
+%plotting
+scatter(X, Y, 5, V, 'filled');
+colorbar;
+
+% Label the axes
+title('Scatter plot with colors based on expression values');
+ % Optionally change the colormap
+
+end
